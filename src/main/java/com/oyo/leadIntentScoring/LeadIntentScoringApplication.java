3d8ff5614a467535181b6466cffcbcd3b42d1519
KrishnaOyo <krishna.murari@oyorooms.com>
2018-10-18 18:37:24 +0530

    first commit

---
+++ src/main/java/com/oyo/leadIntentScoring/LeadIntentScoringApplication.java
@@ -0,0 +1,12 @@
+package com.oyo.leadIntentScoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadIntentScoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadIntentScoringApplication.class, args);
+	}
+}
